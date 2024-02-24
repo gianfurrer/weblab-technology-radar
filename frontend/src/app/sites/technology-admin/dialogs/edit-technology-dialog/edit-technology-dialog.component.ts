64cada3e5b8c582e18b27bbe6a795e5c460804c8
Gian Furrer <gian.furrer@contractors.roche.com>
2024-02-24 14:19:16 +0100

    frotend: add edit technology dialog

---
+++ frontend/src/app/sites/technology-admin/dialogs/edit-technology-dialog/edit-technology-dialog.component.ts
@@ -0,0 +1,74 @@
+import { NgFor, NgIf } from "@angular/common";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { DialogBaseComponent } from "@shared/components/dialog-base.component";
+import { TechnologyService } from "@shared/services/technology/technology.service";
+import { Technology } from "@shared/types/technology";
+import { catchError, firstValueFrom, of, tap } from "rxjs";
+
+@Component({
+  selector: "app-edit-technology-dialog",
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf, NgFor],
+  templateUrl: "./edit-technology-dialog.component.html",
+})
+export class EditTechnologyDialogComponent extends DialogBaseComponent implements OnChanges {
+  @Input() technology?: Technology;
+
+  public editForm?: FormGroup;
+  public submitted = false;
+  public errorMessages = "";
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private technologyService: TechnologyService
+  ) {
+    super();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["technology"] && this.technology) {
+      this.dialog.showModal();
+      this.editForm = this.formBuilder.group({
+        name: [this.technology.name, [Validators.required]],
+        category: [this.technology.category, [Validators.required, Validators.pattern(/(Platforms|Techniques|Tools)/)]],
+        description: [this.technology.description, [Validators.required]],
+      });
+    }
+  }
+
+  public async onSubmit() {
+    this.submitted = true;
+    if (!this.editForm?.valid || !this.technology) {
+      return;
+    }
+
+    const body: Technology = this.editForm.getRawValue();
+    body.id = this.technology.id ?? "";
+    console.log(body);
+
+    await firstValueFrom(
+      this.technologyService.editTechnology(body).pipe(
+        tap((response: Technology) => {
+          if (this.technology) {
+            Object.assign(this.technology, response);
+          }
+          this.close();
+        }),
+        catchError(err => {
+          console.error(err);
+          this.errorMessages = err.error ? (err.error.errors ? err.error.errors : [err.error]) : [err.message];
+          return of(this.errorMessages);
+        })
+      )
+    );
+  }
+
+  public override close() {
+    if (this.dialog.open) {
+      this.dialog.close();
+    }
+    this.submitted = false;
+    this.editForm = undefined;
+  }
+}
--- frontend/src/app/sites/technology-admin/technology-admin.component.ts
+++ frontend/src/app/sites/technology-admin/technology-admin.component.ts
@@ -1,20 +1,22 @@
-import { AsyncPipe, NgFor, NgIf } from "@angular/common";
+import { AsyncPipe, DatePipe, NgFor, NgIf } from "@angular/common";
 import { Component } from "@angular/core";
 import { TechnologyService } from "@shared/services/technology/technology.service";
 import { Technology } from "@shared/types/technology";
 import { Observable } from "rxjs";
 import { PublishDialogComponent } from "./dialogs/publish-dialog/publish-dialog.component";
+import { EditTechnologyDialogComponent } from "./dialogs/edit-technology-dialog/edit-technology-dialog.component";
 
 @Component({
   selector: "app-technology-admin",
   standalone: true,
-  imports: [AsyncPipe, NgIf, NgFor, PublishDialogComponent],
+  imports: [AsyncPipe, DatePipe, NgIf, NgFor, PublishDialogComponent, EditTechnologyDialogComponent],
   templateUrl: "./technology-admin.component.html",
 })
 export class TechnologyAdminComponent {
   public technologies$: Observable<Technology[]>;
 
   public technologyToPublish?: Technology;
+  public technologyToEdit?: Technology;
 
   constructor(private technologyService: TechnologyService) {
     this.technologies$ = this.technologyService.getTechnologies();
@@ -22,5 +24,9 @@ export class TechnologyAdminComponent {
 
   public publishTechnology(technology: Technology) {
     this.technologyToPublish = technology;
+  }
+
+  public editTechnology(technology: Technology) {
+    this.technologyToEdit = technology;
   }
 }

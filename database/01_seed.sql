4293e53d74573c006b2dead9f9a2639bbb93a648
Gian Furrer <gian.furrer@contractors.roche.com>
2024-02-26 09:10:52 +0100

    database: simplify seed script

--- database/01_seed.sql
+++ database/01_seed.sql
@@ -1,34 +1,30 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-WITH
-account_ids AS (
-  INSERT INTO account (email, password, role)
-  VALUES ('cto@technology-radar.ch', crypt('cto-password', gen_salt('bf', 5)), 'CTO'),
-         ('tech-lead@technology-radar.ch', crypt('tech-lead-password', gen_salt('bf', 5)), 'Tech-Lead'),
-         ('user@technology-radar.ch', crypt('user-password', gen_salt('bf', 5)), 'User')
-  RETURNING id, email
-),
-tech AS (
-  INSERT INTO technology (name, category, ring, description, ring_reason, created_by, created_at, published, published_at)
-  SELECT 'Kubernetes', 'Platforms'::category, 'Adopt'::ring, 'Cluster', 'Because Adopt', account_ids.id, NOW(), false, NULL
-  FROM account_ids WHERE email = 'tech-lead@technology-radar.ch'
-  UNION ALL
-  SELECT 'AgroCD', 'Tools'::category, 'Trial'::ring, 'Kubernetes Cluster', 'Because Trial', account_ids.id, NOW(), true, NOW()
-  FROM account_ids WHERE email = 'cto@technology-radar.ch'
-  UNION ALL
-  SELECT 'GitOps', 'Techniques'::category, 'Assess'::ring, 'Technique for deploying Applications', 'Because Assess', account_ids.id, NOW(), false, NULL
-  FROM account_ids WHERE email = 'tech-lead@technology-radar.ch'
-  UNION ALL
-  SELECT 'Web components for SSR', 'Techniques'::category, 'Hold'::ring, 'Server Side Rendering', NULL, account_ids.id, NOW(), false, NULL
-  FROM account_ids WHERE email = 'tech-lead@technology-radar.ch'
-  UNION ALL
-  SELECT 'Design Systems', 'Techniques'::category, 'Adopt'::ring,
-  'As application development becomes increasingly dynamic and complex,
-  its a challenge to deliver accessible and usable products with consistent style.
-  This is particularly true in larger organizations with multiple teams
-  working on different products.', 'Because Adopt', account_ids.id, NOW(), true, NOW()
-  FROM account_ids WHERE email = 'cto@technology-radar.ch'
-  RETURNING id, name, ring, ring_reason, created_by
-)
+INSERT INTO account (email, password, role)
+VALUES ('cto@technology-radar.ch', crypt('cto-password', gen_salt('bf', 5)), 'CTO'),
+       ('tech-lead@technology-radar.ch', crypt('tech-lead-password', gen_salt('bf', 5)), 'Tech-Lead'),
+       ('user@technology-radar.ch', crypt('user-password', gen_salt('bf', 5)), 'User');
+
+INSERT INTO technology (name, category, ring, description, ring_reason, created_by, published, published_at)
+SELECT 'Kubernetes', 'Platforms'::category, 'Adopt'::ring, 'Cluster', 'Because Adopt', account.id, false, NULL
+FROM account WHERE email = 'tech-lead@technology-radar.ch'
+UNION ALL
+SELECT 'AgroCD', 'Tools'::category, 'Trial'::ring, 'Kubernetes Cluster', 'Because Trial', account.id, true, now()::timestamp
+FROM account WHERE email = 'cto@technology-radar.ch'
+UNION ALL
+SELECT 'GitOps', 'Techniques'::category, 'Assess'::ring, 'Technique for deploying Applications', 'Because Assess', account.id, false, NULL
+FROM account WHERE email = 'tech-lead@technology-radar.ch'
+UNION ALL
+SELECT 'Web components for SSR', 'Techniques'::category, 'Hold'::ring, 'Server Side Rendering', NULL, account.id, false, NULL
+FROM account WHERE email = 'tech-lead@technology-radar.ch'
+UNION ALL
+SELECT 'Design Systems', 'Techniques'::category, 'Adopt'::ring,
+'As application development becomes increasingly dynamic and complex,
+its a challenge to deliver accessible and usable products with consistent style.
+This is particularly true in larger organizations with multiple teams
+working on different products.', 'Because Adopt', account.id, true, now()::timestamp
+FROM account WHERE email = 'cto@technology-radar.ch';
+
 INSERT INTO ring_history (technology_id, ring, ring_reason, changed_by, changed_at)
-SELECT tech.id, tech.ring, tech.ring_reason, tech.created_by, NOW()
-FROM tech WHERE ring_reason IS NOT NULL
+SELECT technology.id, technology.ring, technology.ring_reason, technology.created_by, now()::timestamp
+FROM technology WHERE ring_reason IS NOT NULL;
